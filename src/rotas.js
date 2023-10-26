6fdee340fdaa1a443004e55d1694578a87d4f8fb
Tais Costa <taiscosta.eng@gmail.com>
2023-10-26 16:01:13 -0300

    atualização  do arquivo rotas.js

--- src/rotas.js
+++ src/rotas.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const categorias = require('./controladores/categorias');
+const transacoes = require('./controladores/transacoes');
+const usuarios = require('./controladores/usuarios');
 
 const rotas = express();
 
