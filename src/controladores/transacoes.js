6410d649772af507c1182d17fded8147c353343e
Tais Costa <taiscosta.eng@gmail.com>
2023-10-31 13:41:48 -0300

    polimento de rotas de acordo com os requisitos da Cubos Academy

--- src/controladores/transacoes.js
+++ src/controladores/transacoes.js
@@ -11,7 +11,10 @@ const cadastrarTransacao = async (req, res) => {
         const { rows } = await pool.query(`insert into transacoes
         (tipo, descricao, valor, data, categoria_id, usuario_id )
         values ($1, $2, $3, $4, $5, $6) returning *`, [tipo, descricao, valor, data, categoria_id, id])
-        return res.json(rows)
+
+        return res.status(201).json(rows)
+
+        // faltou inserir a  "categoria_nome": "Salários" na resposta
 
     } catch (error) {
         console.log(error)
@@ -27,7 +30,7 @@ const listarTransacoes = async (req, res) => {
         const { id } = req.usuario
 
         const { rows } = await pool.query('select * from transacoes where usuario_id = $1', [id])
-        return res.json(rows)
+        return res.status(200).json(rows)
 
     } catch (error) {
         return res.status(500).json('Erro interno do servidor')
@@ -47,7 +50,10 @@ const detalharTransacao = async (req, res) => {
         const transacaoExiste = await pool.query(`select * from transacoes
         where id = $1 and usuario_id = $2`, [id, id_usuario]);
 
+        console.log(transacaoExiste)
         return res.status(200).json(transacaoExiste.rows)
+
+        // faltou inserir a  "categoria_nome": "Salários" na resposta
 
     } catch (error) {
         return res.status(500).json('Erro interno do servidor')
@@ -62,10 +68,15 @@ const obterExtrato = async (req, res) => {
     try {
         const id_usuario = req.usuario.id
 
-        const resultado = await pool.query(`select * from transacoes
-        where usuario_id = $1`, [id_usuario]);
+        const entrada = await pool.query(`select sum(valor) as entrada from transacoes
+        where usuario_id = $1 and tipo like 'entrada'`, [id_usuario]);
 
-        return res.json(resultado.rows)
+        const saida = await pool.query(`select sum(valor) as saida from transacoes
+        where usuario_id = $1 and tipo like 'saida'`, [id_usuario]);
+
+        const resultado = [entrada.rows[0], saida.rows[0]]
+        // não consegui transformar em um único objeto
+        return res.json(resultado)
 
     } catch (error) {
         return res.status(500).json('Erro interno do servidor')
@@ -119,6 +130,13 @@ const excluirTransacao = async (req, res) => {
     }
 }
 
+// 200 (OK) = requisição bem sucedida
+// 201 (Created) = requisição bem sucedida e algo foi criado
+// 204 (No Content) = requisição bem sucedida, sem conteúdo no corpo da resposta
+// 400 (Bad Request) = o servidor não entendeu a requisição pois está com uma sintaxe/formato inválido
+// 401 (Unauthorized) = o usuário não está autenticado (logado)
+// 403 (Forbidden) = o usuário não tem permissão de acessar o recurso solicitado
+// 404 (Not Found) = o servidor não pode encontrar o recurso solicitado
 
 module.exports = {
     cadastrarTransacao,
